ec1ce5c538f8299317787d8f804453ab58d37ef2
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-10-11 10:23:35 -0300

    created sample

--- UnoRepros/UnoRepros.Shared/MainPage.xaml.cs
+++ UnoRepros/UnoRepros.Shared/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -21,8 +22,39 @@ namespace UnoRepros;
 /// </summary>
 public sealed partial class MainPage : Page
 {
+	public int[] Items = new[] { 0, 1, 2, 3, 4, 6, 7, 8, 9 };
+
 	public MainPage()
 	{
 		this.InitializeComponent();
+		DataContext = this;
+	}
+
+	void ItemClicked(object sender, ItemClickEventArgs e)
+	{
+		if (wasLongPress)
+		{
+			//HACK: workaround to make sure the item will not be selected
+			//await Task.Delay(1);
+			Console.WriteLine($"From the xaml list: {list.SelectedItem}");
+			Console.WriteLine($"From the sender list: {((ListView)sender).SelectedItem}");
+			list.SelectedItem = null;
+			((ListView)sender).SelectedItem = null;
+			wasLongPress = false;
+			return;
+		}
+
+		Console.WriteLine($"You typed on {e.ClickedItem}");
+	}
+
+	bool wasLongPress;
+
+	void OnCellHolding(object sender, HoldingRoutedEventArgs e)
+	{
+		if (e.HoldingState != Microsoft.UI.Input.HoldingState.Started)
+			return;
+
+		Console.WriteLine("Started");
+		wasLongPress = true;
 	}
 }
